00fd48d14839eef8878403e27574666ad4d0ea49
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-12-14 19:05:55 +0100

    Added scripts/

---
+++ quickview-streaming/scripts/mv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Moving core files to project's root $(pwd)."
+
+mv ./quickview-streaming/.gitignore $(pwd)/
+mv ./quickview-streaming/.pre-commit-config.yaml $(pwd)/
+mv ./quickview-streaming/docker-compose.yaml $(pwd)/
+mv ./quickview-streaming/README.md $(pwd)/
